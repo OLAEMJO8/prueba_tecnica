e6eab37dfc6c2191121e5ba7334187e601f8e38d
OLAEMJO8 <olaecheamariajose@gmail.com>
2023-09-02 15:47:07 -0300

    Creacion de rutas y views front

---
+++ frontend/src/views/ProfileInvitaciones.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProfileInvitaciones(props) {
+    return (
+        <div>
+            Ver y eliminar invitaciones
+        </div>
+    );
+}
+
+export default ProfileInvitaciones;
