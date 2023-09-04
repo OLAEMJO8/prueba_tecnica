960001085ebac7f2628fa78adfbfc2283d445910
OLAEMJO8 <olaecheamariajose@gmail.com>
2023-09-04 03:09:08 -0300

    servidor manejo errror

--- src/app.js
+++ src/app.js
@@ -20,4 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api", invitacionesRoutes);
 app.use("/api", authRoutes);
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+  
+
 export default app;
