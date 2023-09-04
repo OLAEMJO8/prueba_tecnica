e8ed5f726a32078624810ddcacd6b045183feb76
OLAEMJO8 <olaecheamariajose@gmail.com>
2023-09-04 02:41:20 -0300

    Areglo rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,37 @@
-
-import {Routes, Route} from "react-router-dom"
-import Home from "./views/Home"
-import Login from './views/Login';
-import Singin from './views/Singin';
-import NewInvitacion from './views/NewInvitacion';
-import ProfileInvitaciones from './views/ProfileInvitaciones';
-import Navbar from './Components/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Singin from "./views/Singin";
+import NewInvitacion from "./views/NewInvitacion";
+import ProfileInvitaciones from "./views/ProfileInvitaciones";
+import Navbar from "./Components/Navbar";
 import { InicioSesion } from "./Components/InicioSesion";
 import { useAuth } from "./context/AuthContext";
 
 function App() {
-  const { isAuth} = useAuth();
+  const { isAuth } = useAuth();
 
   return (
     <>
-    <Navbar/>
-    <div className="max-w-7xl px-4 mx-auto">
+      <Navbar />
 
-    <Routes>
-
-    <Route element={<InicioSesion isAllowed={!isAuth} redirectTo="/profile"/>}>
-      <Route exact path="/home" element={<Home/>}/>
-      <Route exact path="/login" element={<Login/>}/>
-      <Route exact path="/singin" element={<Singin/>}/>
-      
-    </Route>
-    <Route element={<InicioSesion isAllowed={isAuth} redirectTo="/login"/>}>
-     
-      <Route exact path="/profile" element={<ProfileInvitaciones/>}/>
-      <Route exact path="/new" element={<NewInvitacion/>}/>
-      
-    </Route>
-    </Routes>
-    </div>
+      <Routes>
+        <Route
+          element={<InicioSesion isAllowed={!isAuth} redirectTo="/profile" />}
+        >
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/singin" element={<Singin />} />
+        </Route>
+        <Route
+          element={<InicioSesion isAllowed={isAuth} redirectTo="/login" />}
+        >
+          <Route exact path="/profile" element={<ProfileInvitaciones />} />
+          <Route exact path="/new" element={<NewInvitacion />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
