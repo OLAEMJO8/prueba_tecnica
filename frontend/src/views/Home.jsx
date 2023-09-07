ad944c075783dd7aa96011a252c1485ee0ed8fc8
OLAEMJO8 <olaecheamariajose@gmail.com>
2023-09-07 13:28:05 -0300

    Cambios sin marca

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -6,11 +6,7 @@ function Home() {
           Bienvenidos
         </h1>
       </div>
-      <div className="text-center mt-2 flex justify-center items-center">
-        <h1 className="font-bold text-7xl text-[#a7a2a1]">eugenia</h1>
-        <h1 className="font-bold text-xs text-[#000000] mx-2">by</h1>
-        <h1 className="font-bold text-xl text-[#FA573E]">Nextia</h1>
-      </div>
+     
     </div>
   );
 }
